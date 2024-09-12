a01e63abf6e2243524012be89eaadae91aa17fee
Vikas kumar <vikaskumarnandanji@gmail.com>
2024-09-12 10:46:52 +0530

    Final Commint

---
+++ app/src/main/java/com/example/splacescreen/MainActivity2.kt
@@ -0,0 +1,25 @@
+package com.example.splacescreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },2000)
+
+
+    }
+}
